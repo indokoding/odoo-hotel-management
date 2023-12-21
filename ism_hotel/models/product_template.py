7433397e0053d1557d8c8dd2dd5669ec3fc50538
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2023-12-21 11:41:10 +0700

    add functionablity in hotel room type

--- ism_hotel/models/product_template.py
+++ ism_hotel/models/product_template.py
@@ -3,5 +3,5 @@ from odoo import models, fields, api, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     
-    is_room = fields.Boolean(string="Is room")
+    is_room = fields.Boolean(string="Is room", help="Check if this product is a hotel's room type")
     max_allowed_person = fields.Integer(string="Max allowed person")
