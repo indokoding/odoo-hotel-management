bf92a6ebde936b48f2eded77f802be260b68f171
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2023-12-28 16:17:30 +0700

    dashboard view

--- ism_hotel/models/hotel_room.py
+++ ism_hotel/models/hotel_room.py
@@ -4,5 +4,20 @@ class HotelRoom(models.Model):
     _name = 'hotel.room'
     _description = 'Hotel Room'
     
+    STATUS_COLOR = {
+        'available': 'success',
+        'reserved': 'warning',
+        'occupied': 'danger',
+        'maintenance': 'info',
+        'unavailable': 'dark',
+    }
+    
     name = fields.Char(string="Name", required=True)
     room_type = fields.Many2one('product.template', string="Room Type", required=True)
+    state = fields.Selection([
+        ('available', 'Available'),
+        ('reserved', 'Reserved'),
+        ('occupied', 'Occupied'),
+        ('maintenance', 'Maintenance'),
+        ('unavailable', 'Unavailable'),
+    ], string="State", default='available', required=True)
