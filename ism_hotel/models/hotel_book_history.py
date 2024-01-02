85a987b34b3439d49a04380b3c2dcf7bac43852b
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2024-01-02 16:25:19 +0700

    wip check-in

--- ism_hotel/models/hotel_book_history.py
+++ ism_hotel/models/hotel_book_history.py
@@ -11,6 +11,7 @@ class HotelBookHistory(models.Model):
     history_line_ids = fields.One2many('hotel.book.history.line', 'book_history_id', string="History Line")
     check_in = fields.Date(string="Check In", required=True)
     check_out = fields.Date(string="Check Out", required=True)
+    sale_order_id = fields.Many2one('sale.order', string="Sale Order")
     state = fields.Selection([
         ('booked', 'Booked'),
         ('checked_in', 'Checked In'),
@@ -24,20 +25,36 @@ class HotelBookHistory(models.Model):
             vals['name'] = self.env['ir.sequence'].next_by_code('hotel.booking.number') or _('New')
         result = super(HotelBookHistory, self).create(vals)
         
+        sale_order = self._create_sale_order(result)
+        result.sale_order_id = sale_order.id
+        
         # change state of room
         for room in result.room_ids:
             room.state = 'reserved'
-        
-        # create new sale order
+            
+        return True
+    
+    def _create_sale_order(self, result):
         order_lines = []
         room_types = []
         room_type_dict_qty = {}
+        room_type_dict_str_join = {}
+        
         for room in result.room_ids:
             if room.room_type.name not in room_type_dict_qty:
                 room_types.append(room.room_type)
                 room_type_dict_qty[room.room_type.name] = 1
+                room_type_dict_str_join[room.room_type.name] = [room.name]
             else:
                 room_type_dict_qty[room.room_type.name] += 1
+                room_type_dict_str_join[room.room_type.name].append(room.name)
+                
+        # join room name
+        for room_type in room_types:
+            if len(room_type_dict_str_join[room_type.name]) > 1:
+                room_type_dict_str_join[room_type.name] = ', '.join(room_type_dict_str_join[room_type.name])
+            else:
+                room_type_dict_str_join[room_type.name] = room_type_dict_str_join[room_type.name][0]
                 
         for room_type in room_types:
             order_lines.append((0, 0, {
@@ -47,25 +64,14 @@ class HotelBookHistory(models.Model):
                 'product_uom_qty': room_type_dict_qty[room_type.name],
                 'price_unit': room_type.list_price,
             }))
+            order_lines.append((0, 0, {
+                'display_type': 'line_note',
+                'name': room_type.name + ' (' + room_type_dict_str_join[room_type.name] + ')',
+            }))
         
-        sale_order = self.env['sale.order'].create({
+        return self.env['sale.order'].create({
             'partner_id': result.partner_id.id,
             'date_order': result.check_in,
             'order_line': order_lines,
         })
         
-        return result
-        
-    def action_checkin(self):
-        for rec in self:
-            rec.room_booking_id.write({
-                'partner_id': rec.partner_id.id,
-                'check_in': rec.check_in,
-                'check_out': rec.check_out,
-                'room_ids': rec.room_ids,
-                'state': 'checkin',
-            })
-            rec.room_id.write({
-                'state': 'occupied',
-            })
-        return True
--- ism_hotel/wizard/checkin_room_booking.py
+++ ism_hotel/wizard/checkin_room_booking.py
@@ -4,14 +4,61 @@ class CheckinRoomBooking(models.TransientModel):
     _name = 'checkin.room.booking'
     _description = 'Checkin Room Booking'
     
+    @api.model
+    def _get_default_name(self):
+        room_booking = self._get_default_booking()
+        return room_booking.name
+    
+    @api.model
+    def _get_default_room_ids(self):
+        room_booking = self._get_default_booking()
+        return room_booking.room_ids
+        
+    @api.model
+    def _get_default_checkin(self):
+        room_booking = self._get_default_booking()
+        return room_booking.check_in
+    
+    @api.model
+    def _get_default_checkout(self):
+        room_booking = self._get_default_booking()
+        return room_booking.check_out
+    
+    @api.model
+    def _get_default_partner(self):
+        room_booking = self._get_default_booking()
+        return room_booking.partner_id
+    
+    name = fields.Char(string="Name", required=True, copy=False, readonly=True, default=_get_default_name)
+    room_ids = fields.Many2many('hotel.room', string="Selected Room", required=True, default=_get_default_room_ids)
+    check_in = fields.Date(string="Checkin Date", required=True, default=_get_default_checkin)
+    check_out = fields.Date(string="Checkout Date", required=True, default=_get_default_checkout)
+    partner_id = fields.Many2one('res.partner', string="Guest", required=True, default=_get_default_partner)
+    
+    def action_checkin(self):
+        self.ensure_one()
+        room_booking = self._get_default_booking()
+        room_booking.write({
+            'state': 'checked_in',
+            'check_in': self.check_in,
+            'check_out': self.check_out,
+            'room_ids': self.room_ids
+        })
+        
+        for room in self.room_ids:
+            room.state = 'occupied'
+            
+        return {
+            'name': _('Room Booking'),
+            'view_mode': 'form',
+            'res_model': 'hotel.book.history',
+            'res_id': room_booking.id,
+            'type': 'ir.actions.act_window',
+            'target': 'current',
+        }
+    
     def _get_default_booking(self):
         room_id = self.env['hotel.room'].browse(self._context.get('active_id'))
         # look for the first room booking that is available or reserved
-        room_booking = self.env['hotel.book.history'].search([('room_id', '=', room_id.id), ('state', 'in', ['available', 'reserved'])], limit=1)
-        return room_booking.id
-    
-    room_booking_id = fields.Many2one('hotel.book.history', string="Room Booking", default=_get_default_booking)
-    room_id = fields.Many2one('hotel.room', string="Room", required=True)
-    checkin_date = fields.Date(string="Checkin Date", required=True)
-    checkout_date = fields.Date(string="Checkout Date", required=True)
-    partner_id = fields.Many2one('res.partner', string="Guest", required=True)
+        room_booking = self.env['hotel.book.history'].search([('room_ids', 'in', room_id.id), ('state', '=', 'booked')], limit=1)
+        return room_booking
