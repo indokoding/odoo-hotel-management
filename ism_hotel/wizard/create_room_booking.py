77b4067afc03f7e30267305a8b684701365db15d
Ahmad Husein Hambali <ahmadhuseinh.03@gmail.com>
2024-01-02 13:43:10 +0700

    populate booking form

--- ism_hotel/wizard/create_room_booking.py
+++ ism_hotel/wizard/create_room_booking.py
@@ -1,21 +1,19 @@
 from odoo import api, fields, models, _
-
+import datetime
 class CreateRoomBookingWizard(models.TransientModel):
     _name = "create.room.booking.wizard"
     _description = "Create Room Booking Wizard"
 
     @api.model
-    def default_get(self, fields):
-        res = super(CreateRoomBookingWizard, self).default_get(fields)
-        if self._context.get('active_id'):
-            res['partner_id'] = self._context.get('active_id')
-        return res
+    def default_get_room(self):
+        room_id = self.env['hotel.room'].browse(self._context.get('active_id'))
+        print(room_id)
+        return room_id    
 
-    date_reservation = fields.Date(string='Date', required=False)
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     check_in = fields.Date(string="Check In", required=True)
     check_out = fields.Date(string="Check Out", required=True)
-    room_ids = fields.Many2many('hotel.room', string="Room", required=True)
+    room_ids = fields.Many2many('hotel.room', string="Room", required=True, default=default_get_room)
     
     def action_create_room_booking(self):
         self.ensure_one()
@@ -33,3 +31,15 @@ class CreateRoomBookingWizard(models.TransientModel):
             'type': 'ir.actions.act_window',
             'target': 'current',
         }
+        
+    @api.onchange('check_in')
+    def onchange_check_in(self):
+        if self.check_in:
+            if not self.check_out or (self.check_out and self.check_out < self.check_in):
+                self.check_out = self.check_in + datetime.timedelta(days=1)
+                
+    @api.onchange('check_out')
+    def onchange_check_out(self):
+        if self.check_out:
+            if not self.check_in or (self.check_in and self.check_out < self.check_in):
+                self.check_in = self.check_out - datetime.timedelta(days=1)
